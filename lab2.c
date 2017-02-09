137d292d31e2f8593b43e4211a4fc5d4f2c7b8a7
Aaron <apnc7d@mail.missouri.edu>
2017-02-09 10:37:44 -0600

    Version 1.3 for Part 1

--- lab2.c
+++ lab2.c
@@ -93,6 +93,7 @@ void leapYear(int year){
 	//checks if year input was 0
 
 	printf("The value of the input is: %hu",in);
+	printf("There needs to be a way here to test for negative input -- test comment for lab 3");
 
 	if(year == 0){
 		printf("\nYear can not equal 0!\n\n");
